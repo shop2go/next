09b8932f9dd9e521769b7db116412e2fd836c9b8
mmaedel <medienwerksalzburg@gmail.com>
2022-03-10 20:54:34 +0100

    api test

---
+++ api/f.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	//"time"
+
+	f "github.com/fauna/faunadb-go/v5/faunadb"
+)
+
+type DATA map[string]f.Value
+type rv f.RefV
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	var (
+		data DATA
+		rvs  []rv
+	)
+
+	ep := f.Endpoint("https://db.fauna.com:443")
+
+	fdb := os.Getenv("FAUNA_DB")
+
+	c := f.NewFaunaClient(fdb, ep)
+
+	x, err := c.Query(f.Paginate(f.Databases()))
+
+	if err != nil {
+		fmt.Fprint(w, err)
+	}
+
+	//log.Println(x)
+
+	if err = x.Get(&data); err != nil {
+		fmt.Fprint(w, err)
+	}
+
+	x = data["data"]
+
+	if err = x.Get(&rvs); err != nil {
+		fmt.Fprint(w, err)
+	}
+
+	for i := range rvs {
+
+		fmt.Fprint(w, rvs[i].ID)
+	}
+}
