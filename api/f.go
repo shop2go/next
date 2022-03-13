7b2398202814d7cf70a41555c499c4fa794c96c5
mmaedel <medienwerksalzburg@gmail.com>
2022-03-13 10:27:58 +0100

    api test

--- api/f.go
+++ api/f.go
@@ -56,12 +56,21 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<meta http-equiv="X-UA-Compatible" content="ie=edge">
 				<title>CODE2GO</title>
-				<!-- CSS -->
-				<!-- Add Material font (Roboto) and Material icon as needed -->
-				<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Roboto+Mono:300,400,700|Roboto+Slab:300,400,700" rel="stylesheet">
-				<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-				<!-- Add Material CSS, replace Bootstrap CSS -->
-				<link href="https://assets.medienwerk.now.sh/material.min.css" rel="stylesheet">
+				<!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css"
+  rel="stylesheet"
+/>
 				</head>
 				<body style="background-color: #bcbcbc;">
 					   <div class="container" id="data" style="color:white; font-size:30px;">
@@ -79,8 +88,10 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	str = str + `
 	</ul>
 							  </div>
-							  <!-- Then Material JavaScript on top of Bootstrap JavaScript -->
-<script src="https://assets.medienwerk.now.sh/material.min.js"></script>
+							  <script
+							  type="text/javascript"
+							  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"
+							></script>
 							  </body>
 							  </html>`
 
