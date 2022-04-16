49b5164c7ce3f5a968c7e5fb52691a5f4483ffd7
mmaedel <medienwerksalzburg@gmail.com>
2022-04-16 15:55:49 +0200

    api

--- api/data1.go
+++ api/data1.go
@@ -89,13 +89,9 @@ func templ(id string) (GIST, error) {
 
 func Data1(w http.ResponseWriter, r *http.Request) {
 
-	id := r.URL.Hostname()
+	id := r.Host
 
-	id = id + r.URL.Path
-
-	id = strings.TrimSuffix(id, "code2go.dev/api/data1.go")
-
-	id = strings.TrimSuffix(id, ".")
+	//id = id + r.URL.Path
 
 	fmt.Fprint(w, "test: "+id)
 
