cc6389deae0f3685bf5b7017b33663c6c4f5b894
mmaedel <medienwerksalzburg@gmail.com>
2022-04-16 11:10:28 +0200

    api

--- api/data1.go
+++ api/data1.go
@@ -72,58 +72,20 @@ func templ(id string) (GIST, error) {
 
 func Data1(w http.ResponseWriter, r *http.Request) {
 
-	var (
-		data DATA
-		rvs  []rv
-	)
+	id := r.Host
 
-	resp, err := http.Get("https://gist.githubusercontent.com/ssskip/5a94bfcd2835bf1dea52/raw/3b2e5355eb49336f0c6bc0060c05d927c2d1e004/ISO3166-1.alpha2.json")
-	if err != nil {
-		fmt.Fprint(w, err)
-	}
+	id = strings.TrimSuffix(id, "code2go.dev/api/data.go")
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Fprint(w, err)
-	}
+	id = strings.TrimSuffix(id, ".")
 
-	country := make(map[string]string)
+	if id != "" {
 
-	err = json.Unmarshal(body, &country)
-	if err != nil {
-		fmt.Fprint(w, err)
-	}
+		var (
+			data DATA
+			rvs  []rv
+		)
 
-	ep := f.Endpoint("https://db.fauna.com:443")
-
-	fdb := os.Getenv("FAUNA_DB")
-
-	c := f.NewFaunaClient(fdb, ep)
-
-	x, err := c.Query(f.Paginate(f.Databases()))
-	if err != nil {
-		fmt.Fprint(w, err)
-	}
-
-	if err = x.Get(&data); err != nil {
-		fmt.Fprint(w, err)
-	}
-
-	x = data["data"]
-
-	if err = x.Get(&rvs); err != nil {
-		fmt.Fprint(w, err)
-	}
-
-	sort.SliceStable(rvs, func(i, j int) bool {
-		return rvs[i].ID < rvs[j].ID
-	})
-
-	switch r.Method {
-
-	case "POST":
-
-		resp, err := http.Get("https://raw.githubusercontent.com/ovrclk/un-locode/master/data/code-list_json.json")
+		resp, err := http.Get("https://gist.githubusercontent.com/ssskip/5a94bfcd2835bf1dea52/raw/3b2e5355eb49336f0c6bc0060c05d927c2d1e004/ISO3166-1.alpha2.json")
 		if err != nil {
 			fmt.Fprint(w, err)
 		}
@@ -133,164 +95,216 @@ func Data1(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, err)
 		}
 
-		l := make([]LOC, 0)
+		country := make(map[string]string)
 
-		err = json.Unmarshal(body, &l)
+		err = json.Unmarshal(body, &country)
 		if err != nil {
 			fmt.Fprint(w, err)
 		}
 
-		m := make([]LOC, 0)
+		ep := f.Endpoint("https://db.fauna.com:443")
 
-		r.ParseForm()
+		fdb := os.Getenv("FAUNA_DB")
 
-		d := strings.ToUpper(strings.TrimSpace(r.FormValue("data")))
+		c := f.NewFaunaClient(fdb, ep)
 
-		for i := range l {
-
-			if v, ok := country[l[i].CountryCODE]; ok {
-
-				l[i].Country = v
-
-			}
-
-			city := strings.ToUpper(l[i].City)
-
-			if city == d {
-
-				m = append(m, l[i])
-
-				continue
-
-			}
-
-			if strings.Contains(city, "/") {
-
-				n := strings.Split(city, "/")
-
-				for j := range n {
-					if n[j] == d {
-						m = append(m, l[i])
-						break
-					}
-				}
-
-				continue
-
-			}
-
-			if strings.Contains(city, "-") {
-
-				n := strings.Split(city, "-")
-
-				for j := range n {
-					if n[j] == d {
-						m = append(m, l[i])
-						break
-					}
-				}
-
-				continue
-
-			}
-
-			if strings.Contains(city, "(") {
-
-				n := strings.Split(strings.TrimSuffix(city, ")"), "(")
-
-				for j := range n {
-					if n[j] == d {
-						m = append(m, l[i])
-						break
-					}
-				}
-
-				continue
-
-			}
-
-			n := strings.Fields(city)
-
-			k := len(n)
-
-			if k > 1 {
-
-				for j := 0; j < k; j++ {
-
-					if n[j] == d {
-
-						m = append(m, l[i])
-
-						continue
-
-					}
-
-				}
-
-			}
-
-		}
-
-		gist, err := templ(os.Getenv("GIST_ID"))
+		x, err := c.Query(f.Paginate(f.Databases()))
 		if err != nil {
 			fmt.Fprint(w, err)
 		}
 
-		resp, err = http.Get(gist.Files.File2.Raw)
-		if err != nil {
+		if err = x.Get(&data); err != nil {
 			fmt.Fprint(w, err)
 		}
 
-		body, err = ioutil.ReadAll(resp.Body)
-		if err != nil {
+		x = data["data"]
+
+		if err = x.Get(&rvs); err != nil {
 			fmt.Fprint(w, err)
 		}
 
-		sort.SliceStable(m, func(i, j int) bool {
-			return m[i].Country < m[j].Country
+		sort.SliceStable(rvs, func(i, j int) bool {
+			return rvs[i].ID < rvs[j].ID
 		})
 
-		t, err := template.New("data").Parse(string(body))
-		if err != nil {
-			fmt.Fprint(w, err)
-		}
+		switch r.Method {
 
-		t.Execute(w, m)
+		case "POST":
 
-	case "GET":
+			resp, err := http.Get("https://raw.githubusercontent.com/ovrclk/un-locode/master/data/code-list_json.json")
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
 
-		s := make([]string, 0)
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
 
-		for i := range rvs {
+			l := make([]LOC, 0)
 
-			if _, ok := country[rvs[i].ID]; ok {
+			err = json.Unmarshal(body, &l)
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
 
-				s = append(s, country[rvs[i].ID])
+			m := make([]LOC, 0)
+
+			r.ParseForm()
+
+			d := strings.ToUpper(strings.TrimSpace(r.FormValue("data")))
+
+			for i := range l {
+
+				if v, ok := country[l[i].CountryCODE]; ok {
+
+					l[i].Country = v
+
+				}
+
+				city := strings.ToUpper(l[i].City)
+
+				if city == d {
+
+					m = append(m, l[i])
+
+					continue
+
+				}
+
+				if strings.Contains(city, "/") {
+
+					n := strings.Split(city, "/")
+
+					for j := range n {
+						if n[j] == d {
+							m = append(m, l[i])
+							break
+						}
+					}
+
+					continue
+
+				}
+
+				if strings.Contains(city, "-") {
+
+					n := strings.Split(city, "-")
+
+					for j := range n {
+						if n[j] == d {
+							m = append(m, l[i])
+							break
+						}
+					}
+
+					continue
+
+				}
+
+				if strings.Contains(city, "(") {
+
+					n := strings.Split(strings.TrimSuffix(city, ")"), "(")
+
+					for j := range n {
+						if n[j] == d {
+							m = append(m, l[i])
+							break
+						}
+					}
+
+					continue
+
+				}
+
+				n := strings.Fields(city)
+
+				k := len(n)
+
+				if k > 1 {
+
+					for j := 0; j < k; j++ {
+
+						if n[j] == d {
+
+							m = append(m, l[i])
+
+							continue
+
+						}
+
+					}
+
+				}
 
 			}
+
+			gist, err := templ(os.Getenv("GIST_ID"))
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
+
+			resp, err = http.Get(gist.Files.File2.Raw)
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
+
+			body, err = ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
+
+			sort.SliceStable(m, func(i, j int) bool {
+				return m[i].Country < m[j].Country
+			})
+
+			t, err := template.New("data").Parse(string(body))
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
+
+			t.Execute(w, m)
+
+		case "GET":
+
+			s := make([]string, 0)
+
+			for i := range rvs {
+
+				if _, ok := country[rvs[i].ID]; ok {
+
+					s = append(s, country[rvs[i].ID])
+
+				}
+			}
+
+			gist, err := templ(os.Getenv("GIST_ID"))
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
+
+			resp, err = http.Get(gist.Files.File1.Raw)
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
+			//We Read the response body on the line below.
+			body, err = ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
+
+			t, err := template.New("data").Parse(string(body))
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
+
+			t.Execute(w, s)
+
 		}
 
-		gist, err := templ(os.Getenv("GIST_ID"))
-		if err != nil {
-			fmt.Fprint(w, err)
-		}
+	} else {
 
-		resp, err = http.Get(gist.Files.File1.Raw)
-		if err != nil {
-			fmt.Fprint(w, err)
-		}
-		//We Read the response body on the line below.
-		body, err = ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Fprint(w, err)
-		}
-
-		t, err := template.New("data").Parse(string(body))
-		if err != nil {
-			fmt.Fprint(w, err)
-		}
-
-		t.Execute(w, s)
+		http.Redirect(w, r, "https://code2go.dev", http.StatusSeeOther)
 
 	}
 
