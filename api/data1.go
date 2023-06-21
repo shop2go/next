2d17540c70e1fc4c0fa9d83dc9f533dece7bd77a
mmaedel <medienwerksalzburg@gmail.com>
2023-06-21 17:08:18 +0200

    next

--- api/data1.go
+++ api/data1.go
@@ -416,19 +416,3 @@ func Data1(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
-
-func Geo1(w http.ResponseWriter, r *http.Request) {
-
-	resp, err := http.Get("https://vercel-geoip.vercel.app")
-	if err != nil {
-		fmt.Fprint(w, err)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Fprint(w, err)
-	}
-
-	fmt.Fprint(w, body)
-
-}
