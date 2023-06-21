3b2ca6d6477c9fa78f0bd87ed9925499daa742d3
mmaedel <medienwerksalzburg@gmail.com>
2023-06-21 21:32:20 +0200

    next

--- api/data1.go
+++ api/data1.go
@@ -90,15 +90,8 @@ func templ(id string) (GIST, error) {
 func Data1(w http.ResponseWriter, r *http.Request) {
 
 	id := r.Header.Get("x-vercel-ip-country")
-	http.Redirect(w, r, id, 201)
 
-	//fmt.Fprint(w, r.Header.Get("x-vercel-ip-country"))
-
-	id = r.Host
-
-	id = strings.TrimSuffix(id, "code2go.dev")
-
-	id = strings.TrimSuffix(id, ".")
+	fmt.Fprint(w, r.Header.Get("x-vercel-ip-city"))
 
 	country := make(map[string]string)
 
